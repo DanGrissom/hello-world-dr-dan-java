c58a617c5b6b2f2752d602727b2cb1a84412f2d7
Dan Grissom <dgrissom@apu.edu>
2019-12-16 15:22:22 -0800

    Updated template

--- Module_02_Functions_And_Storage/src/Lesson_03_1D_Counting_Array.java
+++ Module_02_Functions_And_Storage/src/Lesson_03_1D_Counting_Array.java
@@ -11,13 +11,9 @@
 //			b) Declaring 1D arrays from a variable
 //			c) Iterating through and printing 1D arrays
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.Random;
-import java.util.Scanner;
-
 public class Lesson_03_1D_Counting_Array {
 
 	// Define a constant int array with bill values
-	private static final int[] BILL_VALS = { 1, 2, 5, 10, 20, 50, 100, 500 };
 
 	///////////////////////////////////////////////////////////////
 	// MAIN - Entry point where code will begin execution for file
@@ -28,44 +24,24 @@ public class Lesson_03_1D_Counting_Array {
 		System.out.println("===========================================================================");
 
 		// Initialize Scanner to read in from user
-		Scanner scan = new Scanner(System.in);
 
 		// Initialize random number generator
-		Random randy = new Random();
 
 		// Prompt user for a number of elements to store
-		System.out.print("\n\nEnter a number of random numbers to generate: ");
-		int numElements = scan.nextInt();
 
 		// Create 1D array from initial size
-		int [] a = new int [numElements];
-		for (int i = 0; i < a.length; i++)
-			a[i] = randy.nextInt(100);
-		int debt = printAndSumArray("Amount I Owe Several Individuals", a);
 
 		// Count the number of each bill		
-		// MAPPING: billCounts[0] = #1s, billCounts[1] = #2s, billCounts[2] = #5s...
-		int [] billCounts = new int[BILL_VALS.length];
 
 		// Iterate through each of the bill values and determine how many we need
-		int remDebt = debt;
-		for (int b = BILL_VALS.length - 1; b >= 0; b--) {
-			// Get next largest bill value
-			int billVal = BILL_VALS[b];
-			int numBillsFittingInDebt = remDebt / billVal;
-			billCounts[b] += numBillsFittingInDebt;
-			remDebt -= (billVal * numBillsFittingInDebt);
-		}
 
 		// Print out summary
-		System.out.printf("\n\nFewest bills required to pay off debt ($%s): \n", debt);
-		for (int i = billCounts.length-1; i > 0; i--)
-			if (billCounts[i] > 0)
-				System.out.printf("\tNumber of $%s bills: %s\n", BILL_VALS[i], billCounts[i]);
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////////
-	// This method creates a new shuffled deck from a new deck.
+	// This prints the array passed in via parameter. In addition, it shows the array
+	// indices and array "name" (also passed in as a parameter) and prints the sum of the
+	// elements of the array.
 	// 		Parameters:
 	//			arrayName - String representing name of array to print
 	//			array - array of ints to print
@@ -73,32 +49,6 @@ public class Lesson_03_1D_Counting_Array {
 	//		Returns:
 	//			An integer representing the sum of the array
 	///////////////////////////////////////////////////////////////////////////////////////
-	private static int printAndSumArray(String arrayName, int [] array) {
-		// Print header
-		System.out.printf("\nPrinting %s (size %s)\n", arrayName, array.length);
-		System.out.println("=================================");
-
-		// Print index numbers
-		for (int i = 0; i < array.length; i++)
-			System.out.printf("a[%s]\t", i);
-
-		// Print = sign for each index/value
-		System.out.println();
-		for (int i = 0; i < array.length; i++)
-			System.out.printf("=\t", i);
-
-		// Print array values
-		System.out.println();
-		int total = 0;
-		for (int i = 0; i < array.length; i++) {
-			System.out.printf("$%s\t", array[i]);
-			total += array[i];
-		}
-
-		// Print and return total
-		System.out.printf("= $%s (TOTAL)", total);
-		return total;
-	}
 
 }
 
