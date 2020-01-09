35171e56b54bc52a82062633e688912326e4600d
Dan Grissom <dgrissom@apu.edu>
2020-01-08 20:58:36 -0800

    Updated template

--- Module_05_Memory_Org_Lambda_Expressions_And_Datastructures/src/Lesson_05_Map_Simple_Values_Countries_Visited_Example.java
+++ Module_05_Memory_Org_Lambda_Expressions_And_Datastructures/src/Lesson_05_Map_Simple_Values_Countries_Visited_Example.java
@@ -13,9 +13,6 @@
 //			d) Normalizing String values for map keys
 //			e) Iterating through map keys and getting associate values
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
 
 public class Lesson_05_Map_Simple_Values_Countries_Visited_Example {
 
@@ -26,34 +23,12 @@ public class Lesson_05_Map_Simple_Values_Countries_Visited_Example {
 		System.out.println("===========================================================================");
 
 		// Initialize Scanner to read in from user
-		Scanner scan = new Scanner(System.in);
 
 		// Initialize data structures and variables
-		Map<String, Integer> mapCountryToVisitorCount = new HashMap<String, Integer>();
-		String country;
 
 		// Add new countries to our set of visited countries until user says "Done"
-		do {
-			// Prompt user for country to visit
-			System.out.print("Please enter a country you have visited (enter \"Done\" to stop): ");
-			country = scan.nextLine().toUpperCase();
-
-			// If the map already contains the country, increment the count
-			if (mapCountryToVisitorCount.containsKey(country)) {
-				int value = mapCountryToVisitorCount.get(country);
-				value++;
-				mapCountryToVisitorCount.put(country, value);
-			}
-			else // Otherwise, set the count to 1 for this country
-				mapCountryToVisitorCount.put(country, 1);
-			
-		} while(!country.equals("DONE"));
-		mapCountryToVisitorCount.remove("DONE");	// Remove the "DONE" from the set since it does not belong
 
 		// Iterate through each unique value in the set (will print in no specific order)
-		System.out.println("Your classmates have visited " + mapCountryToVisitorCount.size() + " unique countries, listed below:");
-		for (String c : mapCountryToVisitorCount.keySet())
-			System.out.println("\t" + c + " had " + mapCountryToVisitorCount.get(c) + " unique visitor(s)");	
 	}
 
 }
