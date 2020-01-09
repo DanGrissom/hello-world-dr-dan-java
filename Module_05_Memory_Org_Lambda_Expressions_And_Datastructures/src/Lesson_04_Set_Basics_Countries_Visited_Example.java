94162cc788a152dc0ab674d33228e192fb36afdf
Dan Grissom <dgrissom@apu.edu>
2020-01-08 20:34:03 -0800

    Updated template

--- Module_05_Memory_Org_Lambda_Expressions_And_Datastructures/src/Lesson_04_Set_Basics_Countries_Visited_Example.java
+++ Module_05_Memory_Org_Lambda_Expressions_And_Datastructures/src/Lesson_04_Set_Basics_Countries_Visited_Example.java
@@ -12,10 +12,6 @@
 //			c) Normalizing String values for sets
 //			d) Iterating through set values
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
-
 public class Lesson_04_Set_Basics_Countries_Visited_Example {
 	public static void main(String[] args) {
 
@@ -24,25 +20,11 @@ public class Lesson_04_Set_Basics_Countries_Visited_Example {
 		System.out.println("===========================================================================");
 
 		// Initialize Scanner to read in from user
-		Scanner scan = new Scanner(System.in);
 		
 		// Initialize data structures and variables
-		Set<String> countriesVisited = new HashSet<String>();
-		String country;
 
 		// Add new countries to our set of visited countries until user says "Done"
-		do {
-			// Prompt the user for country visited and add to set
-			System.out.print("Please enter a country you have visited (enter \"Done\" to stop): ");
-			country = scan.nextLine().toUpperCase();	// Uppercase for consistency
-			countriesVisited.add(country); 	// Will not add duplicate countries
-		} while(!country.equals("DONE"));
-		countriesVisited.remove("DONE");	// Remove the "DONE" from the set since it does not belong
-
 
 		// Iterate through each unique value in the set (will print in no specific order)
-		System.out.println("Your classmates have visited " + countriesVisited.size() + " unique countries, listed below:");
-		for (String c : countriesVisited)
-			System.out.println("\t" + c);		
 	}
 }
