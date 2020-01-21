7f9c1e8d50fe465c0c50668f9c0835ec696c45d0
Dan Grissom <dgrissom@apu.edu>
2020-01-20 19:59:19 -0800

    Templates Updated

--- Module_06_Threads_GUIs_And_APIs/src/main/java/models/Business.java
+++ Module_06_Threads_GUIs_And_APIs/src/main/java/models/Business.java
@@ -13,34 +13,13 @@ package models;
 public class Business {
 	//////////////////////////////////////////////
 	// Instance variables
-	private String name;
-	private double rating;
-	private String address;
-	private String businessId;
 	
 	//////////////////////////////////////////////
 	// Overloaded constructor
-	public Business(String placeName, double placeRating, String placeAddress, String placeId) {
-		super();
-		name = placeName;
-		rating = placeRating;
-		address = placeAddress;
-		businessId = placeId;
-	}
 
 	//////////////////////////////////////////////
 	// Getters
-	public String getName() { return name; }
-	public double getRating() { return rating; }
-	public String getAddress() { return address; }
-	public String getBusinessId() { return businessId; }
 
 	//////////////////////////////////////////////
 	// toString override
-	public String toString() {
-		if (rating > 0)
-			return name + " (" + rating + "/5.0)";
-		else
-			return name + " (NO RATINGS)";
-	}
 }
